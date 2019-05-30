5d9e5f9f5fcfbb96873e6486169c3e69c60f984e
shclub <41466732+shclub@users.noreply.github.com>
2019-05-30 15:02:19 +0900

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -1,2 +1,1 @@
-curl -X GET -H 'Content-Type:application/json'  http://10.103.9.175:5003/ppomppu_monitor -d  '{"param_list" : "ppomppu-anal-service,8000,/postanal/?post=,0,Y,KT,Y,ppomppu-monitoring-db,27017,500660,676003420:AAH7fq_HZzxbmWurz-IWdeUh6vZN1QTQxsE"}'
-
+curl -X GET -H 'Content-Type:application/json'  http://10.110.66.17:8001/ppomppu_monitor -d  '{"param_list" : "ppomppu-anal-service,80,/postanal/?post=,0,Y,KT,Y,ppomppu-monitoring-db,8999,50026600,676003420:AAH7fq_HZzxbmWurz-IWdeUh6vZN1QTQxsE"}'
